777590dde1d1061f570dd4b2bc048aa6d84de45e
Yashraj <yashrajgupta2424@gmail.com>
2022-01-13 15:26:59 +0530

    To-Do commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./maintask.css";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+const App = () => {
+  const [inputList, setinputList] = useState([]);
 
-function App() {
+  const [items, setItems] = useState([]);
+  const itemEvent = (event) => {
+    setinputList(event.target.value);
+  };
+
+  const listItem = () => {
+    setItems((oldarry) => {
+      return [...oldarry, inputList];
+    });
+    setinputList("");
+  };
+
+  const deleteTask = (index) => {
+    var newList = items;
+    newList.splice(index, 1);
+    setItems([...newList]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="maintask">
+      <h1 className="heading">To Do List</h1>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Add Task"
+          className="input"
+          onChange={itemEvent}
+          value={inputList}
+        />
+
+        <button
+          className="btn"
+          onClick={listItem}
+          disabled={inputList.length === 0}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+
+        <ol>
+          {items.map((i, index) => {
+            return (
+              <div>
+                <li className="list">
+                  {i}
+                  <button className="delete" onClick={() => deleteTask(index)}>
+                    <RiDeleteBin6Fill size="20px" />
+                  </button>
+                </li>
+              </div>
+            );
+          })}
+        </ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
